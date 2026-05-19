0be093fe813149d06cef2f7a81dc7f613a596aaa
sashenkaze <sashenkaosazee@gmail.com>
2026-05-19 18:47:46 +0700

    feat: add userexport route

--- be-piketin/routes/user.route.js
+++ be-piketin/routes/user.route.js
@@ -5,8 +5,12 @@ const { checkToken, checkRole } = require('../middlewares/auth')
 const userController = require('../controllers/user.controller')
 const upload = require('../middlewares/upload')
 
+const exceljs = require('exceljs')
+
 router.post('/', checkToken, checkRole('admin'), upload.none(), userController.createUser)
 router.get('/', checkToken, checkRole('admin'), userController.getAllUsers)
+//! route export data user (murid)
+router.get('/export', checkToken, checkRole('admin'), userController.exportUsers) 
 router.get('/:id', checkToken, checkRole('admin'), userController.getUserById)
 router.put('/:id', checkToken, checkRole('admin'), upload.none(), userController.updateUser)
 router.delete('/:id', checkToken, checkRole('admin'), userController.deleteUser)
