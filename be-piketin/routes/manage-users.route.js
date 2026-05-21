1f7acb563867ebe376182c2819deed8f26d51184
sashenkaze <sashenkaosazee@gmail.com>
2026-05-21 21:21:30 +0700

    feat: add manage-users route

---
+++ be-piketin/routes/manage-users.route.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const { checkToken, checkRole } = require('../middlewares/auth')
+const upload = require('../middlewares/upload')
+const manageUsersController = require('../controllers/manage-users.controller')
+
+// administrator CRUD psrayon & kokurikuler
+router.post('/', checkToken, checkRole('administrator'), upload.none(), manageUsersController.createManagedUser)
+router.get('/', checkToken, checkRole('administrator'), manageUsersController.getAllManagedUsers)
+router.get('/:id', checkToken, checkRole('administrator'), manageUsersController.getManagedUserById)
+router.put('/:id', checkToken, checkRole('administrator'), upload.none(), manageUsersController.updateManagedUser)
+router.delete('/:id', checkToken, checkRole('administrator'), manageUsersController.deleteManagedUser)
+
+module.exports = router
