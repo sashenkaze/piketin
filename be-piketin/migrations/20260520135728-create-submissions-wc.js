a6a0bfe2f95b7eef8a662bca8ff2ddc3bb7ee87f
sashenkaze <sashenkaosazee@gmail.com>
2026-05-21 09:04:59 +0700

    feat: add submissionswc table into migration

--- be-piketin/migrations/20260520135728-create-submissions-wc.js
+++ be-piketin/migrations/20260520135728-create-submissions-wc.js
@@ -3,20 +3,19 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('SubmissionWcs', {
+      id: { allowNull: false, autoIncrement: true, primaryKey: true, type: Sequelize.BIGINT },
+      user_id: { allowNull: false, type: Sequelize.BIGINT },
+      reviewed_b: { allowNull: true, type: Sequelize.BIGINT },
+      tanggal_piket: { allowNull: false, type: Sequelize.DATEONLY },
+      tugas: { allowNull: false, type: Sequelize.ENUM('A', 'B') },
+      status: { type: Sequelize.ENUM('Pending','Accepted','Declined'), defaultValue: 'Pending' },
+      kondisi: { type: Sequelize.ENUM('Bersih dan Rapi','Bersih','Kurang Bersih'), allowNull: true },
+      alasan_decline: { type: Sequelize.TEXT, allowNull: true },
+    })
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('users');
   }
 };
