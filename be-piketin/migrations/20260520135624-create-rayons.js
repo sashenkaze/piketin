5f2043d06bfbbae20a04ca320fdcaa50ea1d3b69
sashenkaze <sashenkaosazee@gmail.com>
2026-05-21 08:35:26 +0700

    feat: add rayons table in migration

--- be-piketin/migrations/20260520135624-create-rayons.js
+++ be-piketin/migrations/20260520135624-create-rayons.js
@@ -3,20 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Rayons', {
+      id: { allowNull: false, autoIncrement: true, primaryKey: true, type: Sequelize.BIGINT },
+      nama_rayon: { allowNull: false, type:Sequelize.STRING },
+    })
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('users');
   }
 };
