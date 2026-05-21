50e7e6b7aa13929b8bdc8036d6bed07aff8b8e43
sashenkaze <sashenkaosazee@gmail.com>
2026-05-21 08:50:09 +0700

    feat: add new columns in users migration

--- be-piketin/migrations/20260520135722-alter-users-add-fields.js
+++ be-piketin/migrations/20260520135722-alter-users-add-fields.js
@@ -3,15 +3,38 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn
+    await queryInterface.addColumn('Users', 'rayon_id', {
+      type: Sequelize.BIGINT,
+      allowNull: true,
+      references: { model: 'Rayons', key: "id" }
+    });
+    await queryInterface.addColumn('Users', 'minggu_ke', {
+      type: Sequelize.TINYINT,
+      allowNull: true,  
+    });
+    await queryInterface.addColumn('Users', 'hari_wc', {
+      type: Sequelize.ENUM('Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'),
+      allowNull: true,
+    });
+    await queryInterface.addColumn('Users', 'tugas_wc', {
+      type: Sequelize.ENUM('A', 'B'),
+      allowNull: true,
+    });
+    // update column lama
+    await queryInterface.changeColumn('Users', 'role', {
+      type: Sequelize.ENUM('administrators', 'psrayon', 'kokurikuler', 'murid'),
+      allowNull: false,
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.removeColumn('Users', 'rayon_id');
+    await queryInterface.removeColumn('Users', 'minggu_ke');
+    await queryInterface.removeColumn('Users', 'hari_wc');
+    await queryInterface.removeColumn('Users', 'tugas_wc');
+    await queryInterface.changeColumn('Users', 'role', {
+      type: Sequelize.ENUM('psrayon', 'murid'),
+      allowNull: false,
+    })
   }
 };
