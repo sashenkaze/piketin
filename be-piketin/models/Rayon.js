3a052b31a7b1d88c912412026608e31e18f3ebc6
sashenkaze <sashenkaosazee@gmail.com>
2026-05-21 13:22:08 +0700

    feat: Rayon model

---
+++ be-piketin/models/Rayon.js
@@ -0,0 +1,25 @@
+'use strict';
+const { 
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Rayon extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+        Rayon.hasMany(models.User, { foreignKey: 'rayon_id' });
+    }
+    }
+    Rayon.init({
+    nama_rayon: DataTypes.STRING
+    }, {
+    sequelize,
+    modelName: 'Rayon',
+    timestamps: false // ingat ini karena kita skip createdAt/updatedAt di migration
+    });
+    return Rayon;
+};
