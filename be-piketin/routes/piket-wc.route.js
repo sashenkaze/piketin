58dd840de73801648ed8f0bbe102c6582f036324
sashenkaze <sashenkaosazee@gmail.com>
2026-05-21 21:46:38 +0700

    chore: temporarily commented routes to prevent error

--- be-piketin/routes/piket-wc.route.js
+++ be-piketin/routes/piket-wc.route.js
@@ -4,9 +4,9 @@ const { checkToken, checkRole } = require('../middlewares/auth')
 const upload = require('../middlewares/upload')
 const piketWcController = require('../controllers/piket-wc.controller')
 
-router.post('/', checkToken, checkRole('murid'), upload.none(), piketWcController.createSubmissionWc)
-router.get('/', checkToken, checkRole('kokurikuler'), piketWcController.getAllSubmissionsWc)
-router.get('/my', checkToken, checkRole('murid'), piketWcController.getMySubmissionWc)
-router.put('/:id/status', checkToken, checkRole('kokurikuler'), upload.none(), piketWcController.updateStatusWc)
+// router.post('/', checkToken, checkRole('murid'), upload.none(), piketWcController.createSubmissionWc)
+// router.get('/', checkToken, checkRole('kokurikuler'), piketWcController.getAllSubmissionsWc)
+// router.get('/my', checkToken, checkRole('murid'), piketWcController.getMySubmissionWc)
+// router.put('/:id/status', checkToken, checkRole('kokurikuler'), upload.none(), piketWcController.updateStatusWc)
 
 module.exports = router
--- be-piketin/routes/rayon.route.js
+++ be-piketin/routes/rayon.route.js
@@ -4,10 +4,10 @@ const { checkToken, checkRole } = require('../middlewares/auth')
 const upload = require('../middlewares/upload')
 const rayonController = require('../controllers/rayon.controller')
 
-router.post('/', checkToken, checkRole('administrator'), upload.none(), rayonController.createRayon)
-router.get('/', checkToken, checkRole('administrator'), rayonController.getAllRayon)
-router.get('/:id', checkToken, checkRole('administrator'), rayonController.getRayonById)
-router.put('/:id', checkToken, checkRole('administrator'), upload.none(), rayonController.updateRayon)
-router.delete('/:id', checkToken, checkRole('administrator'), rayonController.deleteRayon)
+// router.post('/', checkToken, checkRole('administrator'), upload.none(), rayonController.createRayon)
+// router.get('/', checkToken, checkRole('administrator'), rayonController.getAllRayon)
+// router.get('/:id', checkToken, checkRole('administrator'), rayonController.getRayonById)
+// router.put('/:id', checkToken, checkRole('administrator'), upload.none(), rayonController.updateRayon)
+// router.delete('/:id', checkToken, checkRole('administrator'), rayonController.deleteRayon)
 
 module.exports = router
