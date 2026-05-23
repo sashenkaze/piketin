aec4ca21dfd139e395696c40dfd77d57dcd24549
sashenkaze <sashenkaosazee@gmail.com>
2026-05-23 15:00:40 +0700

    docs: uncomment managed-users routes

--- be-piketin/routes/manage-users.route.js
+++ be-piketin/routes/manage-users.route.js
@@ -5,10 +5,10 @@ const upload = require('../middlewares/upload')
 const manageUsersController = require('../controllers/manage-users.controller')
 
 //! administrator CRUD psrayon & kokurikuler
-// router.post('/', checkToken, checkRole('administrator'), upload.none(), manageUsersController.createManagedUser)
-// router.get('/', checkToken, checkRole('administrator'), manageUsersController.getAllManagedUsers)
-// router.get('/:id', checkToken, checkRole('administrator'), manageUsersController.getManagedUserById)
-// router.put('/:id', checkToken, checkRole('administrator'), upload.none(), manageUsersController.updateManagedUser)
-// router.delete('/:id', checkToken, checkRole('administrator'), manageUsersController.deleteManagedUser)
+router.post('/', checkToken, checkRole('administrator'), upload.none(), manageUsersController.createManagedUser)
+router.get('/', checkToken, checkRole('administrator'), manageUsersController.getAllManagedUsers)
+router.get('/:id', checkToken, checkRole('administrator'), manageUsersController.getManagedUserById)
+router.put('/:id', checkToken, checkRole('administrator'), upload.none(), manageUsersController.updateManagedUser)
+router.delete('/:id', checkToken, checkRole('administrator'), manageUsersController.deleteManagedUser)
 
 module.exports = router
